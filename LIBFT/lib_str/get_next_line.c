def98bf86c6aac5f02525d4896f7d408e9288b90
agloin <darabeev@gmail.com>
2020-06-29 23:03:20 +0300

    allgit status!

--- LIBFT/lib_str/get_next_line.c
+++ LIBFT/lib_str/get_next_line.c
@@ -12,50 +12,69 @@
 
 #include "libft.h"
 
-static int	write_to_line(char **line, char **statarr)
+int		buffer_handler(char **buffer, char **line)
 {
-	char	*tmp;
+	char	*start;
+	char	*sub;
+	int		len;
+	int		sub_len;
+	int		buf_len;
 
-	CHECKEND((*statarr[0]));
-	if (ft_strchr(*statarr, '\n') != NULL)
+	buf_len = ft_strlen(*buffer);
+	sub_len = 0;
+	if (*buffer && buf_len > 0)
 	{
-		*(ft_strchr(*statarr, '\n')) = '\0';
-		MERROR((*line = ft_strdup(*statarr)));
-		MERROR((tmp = ft_strdup(ft_strchr(*statarr, '\0') + 1)));
-		free(*statarr);
-		MERROR((*statarr = ft_strdup(tmp)));
-		free(tmp);
-	}
-	else
-	{
-		MERROR((*line = ft_strdup(*statarr)));
-		ft_memdel((void **)statarr);
+		start = ft_strchr(*buffer, '\n');
+		if (start)
+			sub_len = ft_strlen(start);
+		len = buf_len - sub_len;
+		sub = ft_strsub(*buffer, 0, len);
+		*line = (len == 0 ? ft_strdup("\0") : ft_strdup(sub));
+		ft_strdel(&sub);
+		if (sub_len > 1)
+			sub = ft_strsub(*buffer, len + 1, buf_len);
+		ft_strdel(buffer);
+		*buffer = sub;
 	}
 	return (1);
 }
 
-int			get_next_line(const int fd, char **line)
+int		key_check(int key, char **dscptr)
 {
-	char		buf[BUFF_SIZE + 1];
-	static char	*statarr[MAX_FD];
-	char		*tmp;
-	int			ret;
-
-	if (fd < 0 || !line || fd > MAX_FD)
+	if (key < 0)
 		return (-1);
-	CHECKMEM(statarr[fd]);
-	while (!ft_strchr(statarr[fd], '\n'))
+	else if (key == 0 && !(*dscptr))
 	{
-		if ((ret = read(fd, buf, BUFF_SIZE)) < 0)
-			return (-1);
-		buf[ret] = '\0';
-		if (ret == 0)
-			break ;
-		MERROR((tmp = ft_strdup(statarr[fd])));
-		free(statarr[fd]);
-		MERROR((statarr[fd] = ft_strjoin(tmp, buf)));
-		CHECKMEM(statarr[fd]);
-		free(tmp);
+		ft_strdel(dscptr);
+		return (0);
 	}
-	return (write_to_line(line, &statarr[fd]));
+	return (1);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char	*dscptr[10245];
+	int			key;
+	char		*tmp;
+	char		buffer[BUFF_SIZE + 1];
+
+	if (fd < 0 || !line || fd > 10240 || BUFF_SIZE <= 0)
+		return (-1);
+	if (dscptr[fd] && ft_strchr(dscptr[fd], '\n'))
+		return (buffer_handler(&dscptr[fd], line));
+	while ((key = read(fd, &buffer, BUFF_SIZE)) && key > 0)
+	{
+		if (!dscptr[fd])
+			dscptr[fd] = ft_strdup("");
+		buffer[key] = '\0';
+		tmp = dscptr[fd];
+		dscptr[fd] = ft_strjoin(dscptr[fd], buffer);
+		ft_strdel(&tmp);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	if (key <= 0)
+		if ((key = key_check(key, &dscptr[fd])) <= 0)
+			return (key);
+	return (buffer_handler(&dscptr[fd], line));
 }
